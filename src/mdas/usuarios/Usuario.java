9aa672f6f09d5eb7a80a4d8c5d57c8eb18e12c22
Veltys <veltys@veltys.es>
2020-04-29 02:44:14 +0200

    ADD: Clase usuario vacía y sus variables privadas

---
+++ src/mdas/usuarios/Usuario.java
@@ -0,0 +1,22 @@
+package mdas.usuarios;
+
+import java.util.Date;
+
+
+/**
+ * Clase Usuario
+ * ...
+ * 
+ * @author	Rafael Carlos Méndez Rodríguez (i82meror)
+ * 
+ * @version	0.0.1
+ */
+
+public abstract class Usuario {
+	private int dni;
+	private String nombre;
+	private String alias;
+	private Date f_nacimiento;
+
+	
+}
