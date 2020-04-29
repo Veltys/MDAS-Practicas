a3ecff290e0d06ee29083a34ce32cc22479e0aff
Veltys <veltys@veltys.es>
2020-04-29 22:26:33 +0200

    ADD: Constructores

--- src/mdas/usuarios/Usuario.java
+++ src/mdas/usuarios/Usuario.java
@@ -1,6 +1,6 @@
 package mdas.usuarios;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 
 /**
@@ -8,15 +8,45 @@ import java.util.Date;
  * ...
  * 
  * @author	Rafael Carlos Méndez Rodríguez (i82meror)
- * 
- * @version	0.0.1
+ * @date	29/04/2020
+ * @version	0.0.2
  */
 
 public abstract class Usuario {
-	private int dni;
-	private String nombre;
-	private String alias;
-	private Date f_nacimiento;
+	private int _dni;
+	private String _nombre;
+	private String _alias;
+	private LocalDate _f_nacimiento;
 
-	
+	/**
+	 * Constructor de clase
+	 * Crea un usuario a partir de su DNI y nombre
+	 * 
+	 * @param	dni								int								DNI
+	 * @param	nombre							String							Nombre
+	 */
+
+	public Usuario(int dni, String nombre) {
+		this._dni			= dni;
+		this._nombre		= nombre;
+		this._alias			= "";
+		this._f_nacimiento	= LocalDate.MIN;
+	}
+
+
+	/**
+	 * Constructor de clase
+	 * Crea un usuario a partir de su DNI, nombre y fecha de nacimiento
+	 * 
+	 * @param	dni								int								DNI
+	 * @param	nombre							String							Nombre
+	 * @param	f_nacimiento					Date							Fecha de nacimiento
+	 */
+
+	public Usuario(int dni, String nombre, LocalDate f_nacimiento) {
+		this._dni			= dni;
+		this._nombre		= nombre;
+		this._alias			= "";
+		this._f_nacimiento	= f_nacimiento;
+	}
 }
