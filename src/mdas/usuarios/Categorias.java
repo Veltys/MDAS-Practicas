11b515a63db0e4164c9ca07306f605ec516209a5
Veltys <veltys@veltys.es>
2020-04-30 16:37:29 +0200

    ADD: Documentación en Javadoc

--- src/mdas/usuarios/Categorias.java
+++ src/mdas/usuarios/Categorias.java
@@ -1,5 +1,16 @@
 package mdas.usuarios;
 
+
+/**
+ * enum Categorias
+ * Enumeración de categorías profesionales para profesores
+ * 
+ * @author			Rafael Carlos Méndez Rodríguez (i82meror)
+ * @date			30/04/2020
+ * @version			1.0.0
+ */
+
+
 public enum Categorias {
 	CATEDRATICO,
 	TITULAR,
