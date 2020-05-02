84ef6f279dd928a94c784a0ea99d38ba6e20c7d1
Veltys <veltys@veltys.es>
2020-05-02 22:17:31 +0200

    ADD: IBuscadorUsuarios

--- src/mdas/usuarios/IBuscadorUsuarios.java
+++ src/mdas/usuarios/IBuscadorUsuarios.java
@@ -1,14 +1,42 @@
 package mdas.usuarios;
 
+import java.util.List;
+
+import mdas.usuarios.Usuario;
+
+
 /**
  * Interfaz IBuscadorUsuarios
  * Interfaz de búsqueda de usuarios
  * 
  * @author			Rafael Carlos Méndez Rodríguez (i82meror)
  * @date			02/05/2020
- * @version			0.0.0
+ * @version			1.0.0
  */
 
 public interface IBuscadorUsuarios {
-	
+	/**
+	 * Método de búsqueda sencuencial de usuarios
+	 * Busca secuencialmente en la lista dada el usuario recibido
+	 * 
+	 * @param		usuarios						List<Usuario>					Lista de usuarios
+	 * @param		buscado							Usuario							Usuario buscado
+	 * 
+	 * @return										int								Su posición en la lista (-1 si no encontrado)
+	 */
+
+	abstract public int BuscadorSeqUsuarios(List<Usuario> usuarios, Usuario buscado);
+
+
+	/**
+	 * Método de búsqueda dicotómica de usuarios
+	 * Busca dicotómicamente en la lista dada el usuario recibido
+	 * 
+	 * @param		usuarios						List<Usuario>					Lista de usuarios
+	 * @param		buscado							Usuario							Usuario buscado
+	 * 
+	 * @return										int								Su posición en la lista (-1 si no encontrado)
+	 */
+
+	abstract public int BuscadorDicUsuarios(List<Usuario> usuarios, Usuario buscado);
 }
