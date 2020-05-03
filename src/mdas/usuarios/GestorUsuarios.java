753722832d4b1067a91ba052cc9c3ebc687293e5
Veltys <veltys@veltys.es>
2020-05-03 21:35:16 +0200

    FIX: Orden de los métodos

--- src/mdas/usuarios/GestorUsuarios.java
+++ src/mdas/usuarios/GestorUsuarios.java
@@ -17,7 +17,7 @@ import mdas.usuarios.Categorias;
  * 
  * @author			Rafael Carlos Méndez Rodríguez (i82meror)
  * @date			03/05/2020
- * @version			0.5.0
+ * @version			0.5.1
  */
 
 public class GestorUsuarios {
@@ -30,86 +30,6 @@ public class GestorUsuarios {
 
 	GestorUsuarios() {
 		this._usuarios = new ArrayList<Usuario>();
-	}
-
-
-	/**
-	 * Método para añadir un usuario a la lista
-	 */
-
-	public void addUsuario() {
-		char		tipo;																// Tipo de usuario a insertar
-		String		dni;																// DNI del usuario a insertar
-		String		nombre;																// Nombre del usuario a insertar
-		String		str_fNacimiento;													// Fecha de nacimiento del usuario a insertar antes de ser convertida al tipo LocalDate
-		LocalDate	fNacimiento			= null;											// Fecha de nacimiento del usuario a insertar ya convertida al tipo LocalDate
-
-		Scanner entrada = new Scanner(System.in);										// Apertura del scanner para lectura por teclado de datos
-
-		System.out.println("¿Qué tipo de usuario se cargará? [A]lumno/[p]rofesor: ");
-		tipo = entrada.next().charAt(0);												// Con recuperar el primer caracter vale
-
-		System.out.println("Introduzca el DNI: ");
-		dni = entrada.next();
-
-		System.out.println("Introduzca el nombre: ");
-		nombre = entrada.next();
-
-		System.out.println("Introduzca la fecha de nacimiento en formato DD/MM/AAAA (opcional, dejar en blanco para continuar): ");
-		str_fNacimiento = entrada.next();
-
-		if(!("".equals(str_fNacimiento))) {
-			fNacimiento = LocalDate.parse(str_fNacimiento, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-		}
-
-		if(Character.toUpperCase(tipo) != 'P') {										// Cualquier cosa que no sea una "P" será tratada como un alumno (el tipo por defecto)
-			int		curso;																// 	Curso del alumno a insertar
-			String	titulacion;															// 	Titulación del alumno a insertar
-
-			System.out.println("Introduzca la titulación: ");
-			titulacion = entrada.next();
-
-			System.out.println("Introduzca el curso: ");
-			curso = entrada.nextInt();
-
-			entrada.close();															// 	Se cierra el scanner, al no necesitarse más
-
-			addAlumno(dni, nombre, fNacimiento, titulacion, curso);
-		}
-		else {																			// Operaciones equivalentes para la insercción de un profesor
-			boolean		categoriaOk = false;											// 	Validador de la categoría profesional
-			int			creditos;														// 	Créditos impartidos del profsor a insertar
-			String		str_categoria;													// 	Categoría profesional del profsor a insertar antes de ser convertida al tipo Categorias
-			Categorias	categoria = null;												// 	Categoría profesional del profsor a insertar ya convertida al tipo Categorias
-
-			System.out.println("Introduzca los créditos impartidos: ");
-			creditos = entrada.nextInt();
-
-			do {																		// 	Validador de categorías profesionales
-				System.out.println("La lista de categorías profesionales disponible es la siguiente:");
-				System.out.println(java.util.Arrays.asList(Categorias.values()));
-				System.out.println("Introduzca la categoría profesional: ");
-				str_categoria = entrada.next();
-
-				for(Categorias c : Categorias.values()) {								// 		Se recorre el enum de categorías para encontrar la proporcionada
-					if(str_categoria.toUpperCase() == c.name()) {						// 			En caso de encontrarla
-						categoriaOk = true;												// 				Se da por válida
-
-						categoria = c;													// 				Se almacena
-
-						break;															// 				El bucle de búsqueda termina
-					}
-				}
-
-				if(!categoriaOk) {														// 		En caso de no ser válida se ha de informar
-					System.out.println("Error: La categoría profesional seleccionada es incorrecta");
-				}
-			} while(!categoriaOk);														// 	El bucle validador sólo acabará cuando se introduzca una categoría válida
-
-			entrada.close();															// 	Se cierra el scanner, al no necesitarse más
-
-			addProfesor(dni, nombre, fNacimiento, creditos, categoria);
-		}
 	}
 
 
@@ -197,7 +117,87 @@ public class GestorUsuarios {
 
 
 	/**
-	 * Método de muestra por pantalla de usuarios
+	 * Método para añadir un usuario a la lista
+	 */
+
+	public void addUsuario() {
+		char		tipo;																// Tipo de usuario a insertar
+		String		dni;																// DNI del usuario a insertar
+		String		nombre;																// Nombre del usuario a insertar
+		String		str_fNacimiento;													// Fecha de nacimiento del usuario a insertar antes de ser convertida al tipo LocalDate
+		LocalDate	fNacimiento			= null;											// Fecha de nacimiento del usuario a insertar ya convertida al tipo LocalDate
+
+		Scanner entrada = new Scanner(System.in);										// Apertura del scanner para lectura por teclado de datos
+
+		System.out.println("¿Qué tipo de usuario se cargará? [A]lumno/[p]rofesor: ");
+		tipo = entrada.next().charAt(0);												// Con recuperar el primer caracter vale
+
+		System.out.println("Introduzca el DNI: ");
+		dni = entrada.next();
+
+		System.out.println("Introduzca el nombre: ");
+		nombre = entrada.next();
+
+		System.out.println("Introduzca la fecha de nacimiento en formato DD/MM/AAAA (opcional, dejar en blanco para continuar): ");
+		str_fNacimiento = entrada.next();
+
+		if(!("".equals(str_fNacimiento))) {
+			fNacimiento = LocalDate.parse(str_fNacimiento, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+		}
+
+		if(Character.toUpperCase(tipo) != 'P') {										// Cualquier cosa que no sea una "P" será tratada como un alumno (el tipo por defecto)
+			int		curso;																// 	Curso del alumno a insertar
+			String	titulacion;															// 	Titulación del alumno a insertar
+
+			System.out.println("Introduzca la titulación: ");
+			titulacion = entrada.next();
+
+			System.out.println("Introduzca el curso: ");
+			curso = entrada.nextInt();
+
+			entrada.close();															// 	Se cierra el scanner, al no necesitarse más
+
+			addAlumno(dni, nombre, fNacimiento, titulacion, curso);
+		}
+		else {																			// Operaciones equivalentes para la insercción de un profesor
+			boolean		categoriaOk = false;											// 	Validador de la categoría profesional
+			int			creditos;														// 	Créditos impartidos del profsor a insertar
+			String		str_categoria;													// 	Categoría profesional del profsor a insertar antes de ser convertida al tipo Categorias
+			Categorias	categoria = null;												// 	Categoría profesional del profsor a insertar ya convertida al tipo Categorias
+
+			System.out.println("Introduzca los créditos impartidos: ");
+			creditos = entrada.nextInt();
+
+			do {																		// 	Validador de categorías profesionales
+				System.out.println("La lista de categorías profesionales disponible es la siguiente:");
+				System.out.println(java.util.Arrays.asList(Categorias.values()));
+				System.out.println("Introduzca la categoría profesional: ");
+				str_categoria = entrada.next();
+
+				for(Categorias c : Categorias.values()) {								// 		Se recorre el enum de categorías para encontrar la proporcionada
+					if(str_categoria.toUpperCase() == c.name()) {						// 			En caso de encontrarla
+						categoriaOk = true;												// 				Se da por válida
+
+						categoria = c;													// 				Se almacena
+
+						break;															// 				El bucle de búsqueda termina
+					}
+				}
+
+				if(!categoriaOk) {														// 		En caso de no ser válida se ha de informar
+					System.out.println("Error: La categoría profesional seleccionada es incorrecta");
+				}
+			} while(!categoriaOk);														// 	El bucle validador sólo acabará cuando se introduzca una categoría válida
+
+			entrada.close();															// 	Se cierra el scanner, al no necesitarse más
+
+			addProfesor(dni, nombre, fNacimiento, creditos, categoria);
+		}
+	}
+
+
+	/**
+	 * Método de mostrado por pantalla de usuarios
 	 * Muestra por pantalla el usuario de la posición dada
 	 * 
 	 * @param		posicion						int								Posición a mostrar
