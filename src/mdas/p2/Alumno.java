835a52de9e4655e34f7cc109b6cd72ce178082f2
Veltys <veltys@veltys.es>
2020-05-17 20:28:54 +0200

    ADD: Clases Alumno y usuario

---
+++ src/mdas/p2/Alumno.java
@@ -0,0 +1,57 @@
+package mdas.p2;
+
+
+import mdas.p2.Usuario;
+
+
+/**
+ * Clase Alumno
+ * Extiende a la clase Usuario para almacenar datos concretos de un alumno
+ *
+ * @author			Rafael Carlos Méndez Rodríguez (i82meror)
+ * @date			17/05/2020
+ * @version			1.0.0
+ */
+
+
+public class Alumno extends Usuario {
+	private String	_correo;
+
+
+	/**
+	 * Constructor de clase
+	 * Crea un alumno a partir de su ID, nombre y correo
+	 *
+	 * @param		id								int								Identificador del alumno
+	 * @param		nombre							String							Nombre del alumno
+	 * @param		correo							String							Correo del alumno
+	 */
+
+	public Alumno(int id, String nombre, String correo) {
+		super(id, nombre);
+
+		this._correo = correo;
+	}
+
+
+	/**
+	 * Observador de la variable privada _correo
+	 *
+	 * @return										String							Correo del alumno
+	 */
+
+	public String correo() {
+		return this._correo;
+	}
+
+
+	/**
+	 * Modificador de la variable privada _correo
+	 *
+	 * @param		correo							String							Correo del alumno
+	 */
+
+	public void correo(String correo) {
+		this._correo = correo;
+	}
+}
---
+++ src/mdas/p2/Usuario.java
@@ -0,0 +1,64 @@
+package mdas.p2;
+
+
+/**
+ * Clase Usuario
+ * Almacena datos de un usuario del sistema
+ * Implementa la interfaz comparable, para facilitar su ordenación
+ *
+ * @author			Rafael Carlos Méndez Rodríguez (i82meror)
+ * @date			17/05/2020
+ * @version			1.0.0
+ */
+
+public class Usuario {
+	private int		_id;
+	private String	_nombre;
+
+
+	/**
+	 * Constructor de clase
+	 * Crea un usuario a partir de su id y su nombre
+	 *
+	 * @param		id								int								Identificador del usuario
+	 * @param		nombre							String							Nombre del usuario
+	 */
+
+	public Usuario(int id, String nombre) {
+		this._id		= id;
+		this._nombre	= nombre;
+	}
+
+
+	/**
+	 * Observador de la variable privada _id
+	 *
+	 * @return										int								Identificador de usuario
+	 */
+
+	public int id() {
+		return this._id;
+	}
+
+
+	/**
+	 * Observador de la variable privada _nombre
+	 *
+	 * @return										String							Nombre
+	 */
+
+	public String nombre() {
+		return this._nombre;
+	}
+
+
+	/**
+	 * Modificador de la variable privada _nombre
+	 *
+	 * @param		nombre							String							Nombre
+	 */
+
+	public void nombre(String nombre) {
+		this._nombre = nombre;
+	}
+}
