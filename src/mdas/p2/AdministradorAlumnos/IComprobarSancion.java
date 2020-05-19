5295e7d629b5ca62eb27738c29fdc09c84d7546c
I72rogah <herrodgar@gmail.com>
2020-05-19 19:41:04 +0200

    ADD: AdministradorAlumno

---
+++ src/mdas/p2/AdministradorAlumnos/IComprobarSancion.java
@@ -0,0 +1,6 @@
+package mdas.p2.AdministradorAlumnos;
+
+public interface IComprobarSancion {
+	public int ComprobarSancion(int id_alumno);
+	public String DescribirSancion(int codigo_sancion);
+}
---
+++ src/mdas/p2/AdministradorAlumnos/IInformarAlumno.java
@@ -0,0 +1,5 @@
+package mdas.p2.AdministradorAlumnos;
+
+public interface IInformarAlumno {
+	public void MostrarMensaje(String mensaje);
+}
