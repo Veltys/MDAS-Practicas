5072bc15706171e92eb4a51d86706a906106ce46
Veltys <veltys@veltys.es>
2020-05-26 23:18:13 +0200

    ADD: Parte de la funcionalidad de reservar

--- src/mdas/p2/main/MenuPrincipal.java
+++ src/mdas/p2/main/MenuPrincipal.java
@@ -2,11 +2,14 @@ package mdas.p2.main;
 
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 import mdas.p2.administradoralumnos.AdministradorAlumnos;
 import mdas.p2.administradorusuarios.AdministradorUsuarios;
-// import mdas.p2.gestorsalas.GestorSalas;
+import mdas.p2.gestorreservamgr.Recurso;
+import mdas.p2.gestorsalas.GestorSalas;
 
 
 /**
@@ -15,7 +18,7 @@ import mdas.p2.administradorusuarios.AdministradorUsuarios;
  *
  * @author			Rafael Carlos Méndez Rodríguez (i82meror)
  * @date			25/05/2020
- * @version			0.3.1
+ * @version			0.4.0 (alfa)
  */
 
 public class MenuPrincipal {
@@ -44,7 +47,6 @@ public class MenuPrincipal {
 		int						idSancion;
 		AdministradorAlumnos	aa			= new AdministradorAlumnos(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES, MenuPrincipal.ARCHIVOUSUARIOS);
 		AdministradorUsuarios	au			= new AdministradorUsuarios(MenuPrincipal.ARCHIVOUSUARIOS);
-		// GestorSalas				gs			= new GestorSalas(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES);
 
 		System.out.println("Bienvenido al Gestor de salas");
 
@@ -59,36 +61,125 @@ public class MenuPrincipal {
 			}
 		}
 
-		while(!salir) {
+		if(!salir) {
 			System.out.println("¡Bienvenido, " + au.nombre(idUsuario) + "!");
 
-			if(au.alumno(idUsuario)) {
-				if((idSancion = aa.comprobarSancion(idUsuario)) == -1) {
+			while(!salir) {
+				if(au.alumno(idUsuario)) {
+					if((idSancion = aa.comprobarSancion(idUsuario)) == -1) {
+						System.out.println("El menú de operaciones es el siguiente:");
+
+						System.out.println("Introduzca 'r' para realizar una reserva");
+						System.out.println("Introduzca 's' para salir");
+
+						System.out.print("Por favor, seleccione una operación para continuar [rs]: ");
+					}
+					else {
+						System.out.println(aa.notificarAlumnoSancionado(idUsuario, idSancion));
+
+						salir = true;
+					}
+				}
+				else if(au.empleado(idUsuario)) {
 					System.out.println("El menú de operaciones es el siguiente:");
 
-					// FIXME: Evitando, por ahora, un bucle infinito
-					salir = true;
+					System.out.println("Introduzca 's' para salir");
+
+					System.out.print("Por favor, seleccione una operación para continuar [s]: ");
 				}
 				else {
-					System.out.println(aa.notificarAlumnoSancionado(idUsuario, idSancion));
+					System.out.println("Lo sentimos, su perfil de usuario no le permite utilizar este servicio");
 
 					salir = true;
 				}
-			}
-			else if(au.empleado(idUsuario)) {
-				System.out.println("El menú de operaciones es el siguiente:");
 
-				// FIXME: Evitando, por ahora, un bucle infinito
-				salir = true;
-			}
-			else {
-				System.out.println("Lo sentimos, su perfil de usuario no le permite utilizar este servicio");
+				if(!salir) {
+					operacion = Character.toUpperCase(MenuPrincipal._entrada.next().charAt(0));
 
-				salir = true;
+					switch(operacion) {
+					case 'R':
+						MenuPrincipal.reserva(idUsuario);
+
+						break;
+
+					case 'S':
+						salir = true;
+
+						break;
+
+					default:
+						System.out.println("La operación seleccionada (" + operacion + ") es incorrecta");
+						System.out.println("Por favor, seleccione una válida del menú");
+
+						break;
+					}
+				}
 			}
 		}
 
 		System.out.println("Gracias por utilizar nuestro sistema");
 		System.out.println("Saliendo...");
+	}
+
+
+	// TODO: Comentar
+
+	static private void reserva(int idAlumno) {
+		/*
+		private int				_idSala;
+		private int				_duracion;
+		private String			_asignatura;
+		private LocalDateTime	_fechaYHora;
+		 */
+
+		// int					alumnos;
+		ArrayList<Integer>	idsRecursos				= new ArrayList<Integer>();
+		ArrayList<Integer>	idsRecursosIncorrectos	= new ArrayList<Integer>();
+		ArrayList<Integer>	idsRecursosPedidos		= new ArrayList<Integer>();
+		ArrayList<Recurso>	recursos;
+		String				linea;
+		GestorSalas			gs						= new GestorSalas(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES);
+		StringTokenizer		stLinea;
+
+
+		System.out.println("Para realizar una reserva es necesario introducir datos que se le irán solicitando uno por uno");
+		System.out.print("En primer lugar, introduzca las personas que ocuparán la sala reservada: ");
+
+		/* alumnos = */ MenuPrincipal._entrada.nextInt();
+
+		System.out.println("A continuación se le presentarán una serie de recursos disponibles");
+
+		recursos = gs.obtenerRecursos();
+
+		for(Recurso r: recursos) {
+			System.out.println(r.id() + ": " + r.nombre() + " (" + r.descripcion() + ")");
+
+			idsRecursos.add(r.id());
+		}
+
+		MenuPrincipal._entrada.nextLine();
+
+		System.out.print("Seleccione tantos como necesite, introduciendo sus números, separados por espacios: ");
+		linea = MenuPrincipal._entrada.nextLine();
+
+		stLinea = new StringTokenizer(linea, " ");
+
+		while(stLinea.hasMoreTokens()) {
+			idsRecursosPedidos.add(Integer.parseInt(stLinea.nextToken()));
+		}
+
+		for(Integer idRecursoPedido: idsRecursosPedidos) {
+			if(!idsRecursos.contains(idRecursoPedido)) {
+				System.out.println("El número de recurso " + idRecursoPedido + " es incorrecto y no será tomado en cuenta");
+
+				idsRecursosIncorrectos.add(idRecursoPedido);
+			}
+		}
+
+		for(Integer idRecursoIncorrecto: idsRecursosIncorrectos) {
+			idsRecursosPedidos.remove(idRecursoIncorrecto);
+		}
+
+		// TODO: Seguir aquí
 	}
 }
