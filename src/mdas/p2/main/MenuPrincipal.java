f2d6a6320e5266610f75cc4a58fb43063445ff34
Veltys <veltys@veltys.es>
2020-05-28 18:23:06 +0200

    FIX: Refactorización de métodos y variables
    Nota: Versión 1.0.0 implica un cambio de versión mayor, pero no implica que esté finalizado el programa; el estado final será alcanzado, pues, en la versión 2.0.0

--- src/mdas/p2/main/MenuPrincipal.java
+++ src/mdas/p2/main/MenuPrincipal.java
@@ -20,95 +20,70 @@ import mdas.p2.gestorsalas.GestorSalas;
  *
  * @author		Rafael Carlos Méndez Rodríguez (i82meror)
  * @date		28/05/2020
- * @version		0.4.3
+ * @version		1.0.0
  */
 
 public class MenuPrincipal {
-	final static private	String		ARCHIVOUSUARIOS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "usuarios.csv";
-	final static private	String		ARCHIVOINCIDENCIAS		= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "incidencias.csv";
-	final static private	String		ARCHIVORECURSOS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "recursos.csv";
-	final static private	String		ARCHIVORESERVAS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "reservas.csv";
-	final static private	String		ARCHIVOSALAS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "salas.csv";
-	final static private	String		ARCHIVOSALASYRECURSOS	= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "salasyrecursos.csv";
-	final static private	String		ARCHIVOSANCIONES		= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "sanciones.csv";
+	final static private	String						ARCHIVOUSUARIOS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "usuarios.csv";
+	final static private	String						ARCHIVOINCIDENCIAS		= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "incidencias.csv";
+	final static private	String						ARCHIVORECURSOS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "recursos.csv";
+	final static private	String						ARCHIVORESERVAS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "reservas.csv";
+	final static private	String						ARCHIVOSALAS			= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "salas.csv";
+	final static private	String						ARCHIVOSALASYRECURSOS	= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "salasyrecursos.csv";
+	final static private	String						ARCHIVOSANCIONES		= System.getProperty("user.dir") + File.separatorChar + "data" + File.separatorChar + "sanciones.csv";
 
-
-	private static			Scanner		_entrada		= new Scanner(System.in);
+	private static			Scanner					_entrada					= new Scanner(System.in);
+	private static			AdministradorAlumnos	_aa							= new AdministradorAlumnos(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES, MenuPrincipal.ARCHIVOUSUARIOS);
+	private static			AdministradorUsuarios	_au							= new AdministradorUsuarios(MenuPrincipal.ARCHIVOUSUARIOS);
+	private static			GestorSalas				_gs							= new GestorSalas(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES);
 
 
 	/**
-	 * Método main de la clase (y del programa)
+	 * Método estático privado de inicio de sesión
 	 *
-	 * @param		args							String							Argumentos recibidos por la línea de comandos
-	 *
-	 * TODO: Reducir longitud
+	 * @return										int								ID del usuario (-1 si no ha iniciado sesión)
 	 */
 
-	public static void main(String[] args) {
-		boolean					salir		= false;
-		char					operacion;
-		int						idUsuario	= -1;
-		int						idReserva;
-		int						idSancion;
-		String					operaciones;
-		ArrayList<Integer>		idsReservas	= null;
-		AdministradorAlumnos	aa			= new AdministradorAlumnos(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES, MenuPrincipal.ARCHIVOUSUARIOS);
-		AdministradorUsuarios	au			= new AdministradorUsuarios(MenuPrincipal.ARCHIVOUSUARIOS);
-		GestorSalas				gs			= new GestorSalas(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES);
+	static private int iniciarSesion() {
+		char					confirmacion	= 'S';
+		int						idUsuario		= -1;
 
-		System.out.println("Bienvenido al Gestor de salas");
-
-		while(!salir && ((idUsuario = au.iniciarSesion()) == -1)) {
+		while((Character.toUpperCase(confirmacion) == 'S') && ((idUsuario = MenuPrincipal._au.iniciarSesion()) == -1)) {
 			System.out.println("Ha introducido unas credenciales de usuario no válidas");
 			System.out.print("¿Desea volverlo a intentar? [S/n]: ");
 
-			operacion = Character.toUpperCase(MenuPrincipal._entrada.next().charAt(0));
+			confirmacion = Character.toUpperCase(MenuPrincipal._entrada.next().charAt(0));
+		}
 
-			if(Character.toUpperCase(operacion) == 'N') {
-				salir = true;
-			}
+		return idUsuario;
+	}
+
+
+	/**
+	 * Método estático main de la clase (y del programa)
+	 *
+	 * @param		args							String							Argumentos recibidos por la línea de comandos
+	 */
+
+	static public void main(String[] args) {
+		boolean					salir		= false;
+		char					operacion;
+		int						idUsuario	= -1;
+		String					operaciones;
+
+		System.out.println("Bienvenido al Gestor de salas");
+
+		if((idUsuario = MenuPrincipal.iniciarSesion()) == -1) {
+			salir = true;
 		}
 
 		if(!salir) {
-			System.out.println("¡Bienvenido, " + au.nombre(idUsuario) + "!");
+			System.out.println("¡Bienvenido, " + MenuPrincipal._au.nombre(idUsuario) + "!");
 
 			while(!salir) {
-				operaciones	= "";
+				operaciones = MenuPrincipal.menu(idUsuario);
 
-				if(au.alumno(idUsuario)) {
-					if((idSancion = aa.comprobarSancion(idUsuario)) == -1) {
-						System.out.println("El menú de operaciones es el siguiente:");
-						System.out.println("Introduzca 'r' para realizar una reserva");
-
-						operaciones += 'r';
-
-						if((idsReservas = gs.buscarReservas(idUsuario, false)) != null) {
-							System.out.println("Introduzca 'm' para modificar una reserva ya existente");
-
-							operaciones += 'm';
-						}
-
-						System.out.println("Introduzca 's' para salir");
-
-						operaciones += 's';
-					}
-					else {
-						System.out.println(aa.notificarAlumnoSancionado(idUsuario, idSancion));
-
-						salir = true;
-					}
-				}
-				else if(au.empleado(idUsuario)) {
-					System.out.println("El menú de operaciones es el siguiente:");
-					System.out.println("Introduzca 's' para salir");
-
-					operaciones += 's';
-				}
-				else {
-					System.out.println("Lo sentimos, su perfil de usuario no le permite utilizar este servicio");
-
-					salir = true;
-				}
+				salir = ("".equals(operaciones));
 
 				if(!salir) {
 					System.out.print("Por favor, seleccione una operación para continuar [" + operaciones + "]: ");
@@ -118,34 +93,8 @@ public class MenuPrincipal {
 					if(operaciones.toUpperCase().indexOf(operacion) != -1) {
 						switch(operacion) {
 						case 'M':
-							if((idReserva = MenuPrincipal.seleccionarReserva(idsReservas)) != -1) {
-								switch(gs.suspenderReserva(idUsuario, idReserva)) {
-								default:
-									if(MenuPrincipal.reservar(idUsuario)) {
-										gs.eliminarReserva(idReserva);
+							MenuPrincipal.modificarReserva(idUsuario);
 
-										System.out.println("La reserva ha sido modificada satisfactoriamente");
-									}
-									else {
-										gs.reanudarReserva(idUsuario, idReserva);
-
-										System.out.println("La reserva no ha sido modificada");
-									}
-
-									break;
-								case -1:
-									System.out.println("La reserva seleccionada (" + idReserva + ") no existe");
-
-									break;
-								case -2:
-									System.out.println("La reserva seleccionada (" + idReserva + ") no es de su propiedad");
-
-									break;
-								}
-							}
-							else {
-								System.out.println("Operación cancelada");
-							}
 							break;
 						case 'R':
 							MenuPrincipal.reservar(idUsuario);
@@ -165,7 +114,6 @@ public class MenuPrincipal {
 						System.out.println("La operación seleccionada (" + operacion + ") es incorrecta o no tiene permisos para realizarla");
 						System.out.println("Por favor, seleccione una válida del menú");
 					}
-
 				}
 			}
 		}
@@ -176,7 +124,94 @@ public class MenuPrincipal {
 
 
 	/**
-	 * Método privado para seleccionar una reserva
+	 * Método estático privado para mostrar el menú del programa
+	 *
+	 * @param		idUsuario						int								ID del usuario que utiliza el programa
+	 *
+	 * @return										String							Operaciones disponibles para ese usuario ("" si ninguna)
+	 */
+
+	static String menu(int idUsuario) {
+		int		idSancion;
+		String	operaciones	= "";
+
+		if(MenuPrincipal._au.alumno(idUsuario)) {
+			if((idSancion = MenuPrincipal._aa.comprobarSancion(idUsuario)) == -1) {
+				System.out.println("El menú de operaciones es el siguiente:");
+				System.out.println("Introduzca 'r' para realizar una reserva");
+
+				operaciones += 'r';
+
+				if(MenuPrincipal._gs.buscarReservas(idUsuario, false) != null) {
+					System.out.println("Introduzca 'm' para modificar una reserva ya existente");
+
+					operaciones += 'm';
+				}
+
+				System.out.println("Introduzca 's' para salir");
+
+				operaciones += 's';
+			}
+			else {
+				System.out.println(MenuPrincipal._aa.notificarAlumnoSancionado(idUsuario, idSancion));
+			}
+		}
+		else if(MenuPrincipal._au.empleado(idUsuario)) {
+			System.out.println("El menú de operaciones es el siguiente:");
+			System.out.println("Introduzca 's' para salir");
+
+			operaciones += 's';
+		}
+		else {
+			System.out.println("Lo sentimos, su perfil de usuario no le permite utilizar este servicio");
+		}
+
+		return operaciones;
+	}
+
+
+	/**
+	 * Método estático privado para modificar una reserva
+	 *
+	 * @param		idAlumno						int								ID del alumno que solicita modificar la reserva
+	 */
+
+	static private void modificarReserva(int idAlumno) {
+		int idReserva = MenuPrincipal.seleccionarReserva(MenuPrincipal._gs.buscarReservas(idAlumno, false));
+
+		if(idReserva != -1) {
+			switch(MenuPrincipal._gs.suspenderReserva(idAlumno, idReserva)) {
+			default:
+				if(MenuPrincipal.reservar(idAlumno)) {
+					MenuPrincipal._gs.eliminarReserva(idReserva);
+
+					System.out.println("La reserva ha sido modificada satisfactoriamente");
+				}
+				else {
+					MenuPrincipal._gs.reanudarReserva(idAlumno, idReserva);
+
+					System.out.println("La reserva no ha sido modificada");
+				}
+
+				break;
+			case -1:
+				System.out.println("La reserva seleccionada (" + idReserva + ") no existe");
+
+				break;
+			case -2:
+				System.out.println("La reserva seleccionada (" + idReserva + ") no es de su propiedad");
+
+				break;
+			}
+		}
+		else {
+			System.out.println("Operación cancelada");
+		}
+	}
+
+
+	/**
+	 * Método estático privado para seleccionar una reserva
 	 *
 	 * @param		idsReservas						ArrayList&lt;Integer&gt;		Lista de IDs de reservas asociadas al alumno
 	 *
@@ -184,15 +219,13 @@ public class MenuPrincipal {
 	 */
 
 	static private int seleccionarReserva(ArrayList<Integer> idsReservas) {
-		GestorSalas gs = new GestorSalas(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES);
-
 		String opcion;
 
 		System.out.println("A continuación se le presentarán todas las reservas pendientes que ha realizado");
 
 		for(int idReserva: idsReservas) {
 			System.out.println("Reserva " + idReserva + ":");
-			System.out.println(gs.mostrarReserva(idReserva));
+			System.out.println(MenuPrincipal._gs.mostrarReserva(idReserva));
 		}
 
 		System.out.print("Seleccione la que desea modificar (enter para cancelar): ");
@@ -211,7 +244,7 @@ public class MenuPrincipal {
 
 
 	/**
-	 * Método privado para reservar una sala
+	 * Método estático privado para reservar una sala
 	 *
 	 * @param		idAlumno						int								ID del alumno que reserva la sala
 	 *
@@ -233,7 +266,6 @@ public class MenuPrincipal {
 		String				linea;
 		LocalDateTime		ahora;
 		LocalDateTime		fechaYHora				= null;
-		GestorSalas			gs						= new GestorSalas(MenuPrincipal.ARCHIVOINCIDENCIAS, MenuPrincipal.ARCHIVORECURSOS, MenuPrincipal.ARCHIVORESERVAS, MenuPrincipal.ARCHIVOSALAS, MenuPrincipal.ARCHIVOSALASYRECURSOS, MenuPrincipal.ARCHIVOSANCIONES);
 		StringTokenizer		stLinea;
 
 
@@ -244,10 +276,10 @@ public class MenuPrincipal {
 
 		System.out.println("A continuación se le presentarán una serie de recursos disponibles");
 
-		idsRecursos = gs.obtenerRecursos();
+		idsRecursos = MenuPrincipal._gs.obtenerRecursos();
 
 		for(int idRecurso: idsRecursos) {
-			System.out.println(gs.mostrarRecurso(idRecurso));
+			System.out.println(MenuPrincipal._gs.mostrarRecurso(idRecurso));
 		}
 
 		MenuPrincipal._entrada.nextLine();
@@ -273,7 +305,7 @@ public class MenuPrincipal {
 			idsRecursosPedidos.remove(idRecursoIncorrecto);
 		}
 
-		idsSalasCandidatas = gs.buscarSala(alumnos, idsRecursosPedidos);
+		idsSalasCandidatas = MenuPrincipal._gs.buscarSala(alumnos, idsRecursosPedidos);
 
 		if(idsSalasCandidatas != null) {
 			System.out.print("Introduzca la asignatura para la que será reservada la sala: ");
@@ -307,19 +339,19 @@ public class MenuPrincipal {
 
 			duracion = MenuPrincipal._entrada.nextInt();
 
-			if((idReserva = gs.elegirSala(alumnos, idAlumno, asignatura, duracion, fechaYHora, idsSalasCandidatas)) != -1) {
-				System.out.println("La sala más adecuada para sus requisitos es:" + System.getProperty("line.separator") + gs.mostrarReserva(idReserva));
+			if((idReserva = MenuPrincipal._gs.elegirSala(alumnos, idAlumno, asignatura, duracion, fechaYHora, idsSalasCandidatas)) != -1) {
+				System.out.println("La sala más adecuada para sus requisitos es:" + System.getProperty("line.separator") + MenuPrincipal._gs.mostrarReserva(idReserva));
 				System.out.print("¿Está de acuerdo? [S/n]: ");
 
 				if(Character.toUpperCase(MenuPrincipal._entrada.next().charAt(0)) != 'N') {
-					gs.confirmarReserva(idReserva);
+					MenuPrincipal._gs.confirmarReserva(idReserva);
 
 					System.out.println("La reserva se ha confirmado");
 
 					return true;
 				}
 				else {
-					gs.eliminarReserva(idReserva);
+					MenuPrincipal._gs.eliminarReserva(idReserva);
 
 					System.out.println("La reserva se ha cancelado");
 
